37798909db59c0c4263a0c1e51bcbeef7fb33baa
loarsaw <amanahmed1385@gmail.com>
2025-12-23 13:06:25 +0530

    fix: correct MuJoCo library linking and include paths in CMakeLists.txt

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/suprajs/cpp_mujoco
+# Install script for directory: /home/nameless/supraj/mujoco_impl
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/suprajs/cpp_mujoco/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nameless/supraj/mujoco_impl/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- prac.c
+++ prac.c
@@ -1,27 +1,23 @@
-#include "mujoco.h"
-#include "stdio.h"
+// correct way to import based on your step up
+// try to download, extract and place the them in usr/local 
+#include <mujoco/mujoco.h>
+#include <stdio.h>
 
 char error[1000];
-myModel* m;
-myData* d;
+mjModel* m;  
+mjData* d;  
 
 int main(void){
-
-m = my_loadXML("hello.xml",NULL,error,1000);
-if(!m){
-printf("\n%s\n",error);
-return 1;
-}
-
-d = mj_makeData(m);
-
-while(d->time < 10){
-mj_step(m,d);
-}
-
-mj_deleteData(d);
-mj_deleteModel(m);
-
-return 0;
-
+    m = mj_loadXML("hello.xml", NULL, error, 1000); 
+    if(!m){
+        printf("\n%s\n", error);
+        return 1;
+    }
+    d = mj_makeData(m);
+    while(d->time < 10){
+        mj_step(m, d);
+    }
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    return 0;
 }
